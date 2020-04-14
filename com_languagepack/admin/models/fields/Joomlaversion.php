dce0df185b6a8109daa539e8cc525b510474f8af
wilsonge <georgejameswilson@googlemail.com>
2020-04-14 11:46:50 +0100

    Joomla versions

---
+++ com_languagepack/admin/models/fields/Joomlaversion.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\FormHelper;
+use Joomla\CMS\Language\Text;
+
+FormHelper::loadFieldClass('list');
+
+/**
+ * Provides a list of versions
+ *
+ * @since  1.0
+ */
+class LanguagepackFormFieldJoomlaversion extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.0
+	 */
+	public $type = 'Joomlaversion';
+
+	/**
+	 * Cached array of the table data.
+	 *
+	 * @var    array
+	 * @since  1.0
+	 */
+	protected static $items = null;
+
+	/**
+	 * Method to get the field options for versions.
+	 *
+	 * @return  array  The options the field is going to show.
+	 *
+	 * @since   1.0
+	 */
+	public function getOptions()
+	{
+		if (empty(static::$items))
+		{
+			$db    = Factory::getDbo();
+
+			$query = $db->getQuery(true)
+				->select($db->quoteName(array('id','name'), array('value', 'text')))
+				->from($db->quoteName('#__languagepack_jversions'));
+
+			$db->setQuery($query);
+
+			// Load the options then translate the version strings
+			$versions = $db->loadAssocList();
+			$translatedItems = array();
+
+			foreach ($versions as $version)
+			{
+				$translatedItem = new \stdClass;
+				$translatedItem->text = Text::_($version['text']);
+				$translatedItem->value = $version['value'];
+
+				$translatedItems[] = $translatedItem;
+			}
+
+			static::$items = $translatedItems;
+		}
+
+		return array_merge(parent::getOptions(), static::$items);
+	}
+}
--- com_languagepack/admin/sql/install/install_mysql.sql
+++ com_languagepack/admin/sql/install/install_mysql.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `#__languagepack_sources` (
 -- Table structure for table `#__languagepack_jversions`
 --
 CREATE TABLE IF NOT EXISTS `#__languagepack_jversions` (
-  `id` INT(3) unsigned NOT NULL,
+  `id` INT(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
@@ -25,13 +25,13 @@ CREATE TABLE IF NOT EXISTS `#__languagepack_languages` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `lang_code` VARCHAR(5) NOT NULL,
-  `jversion_id` INT(3) unsigned NOT NULL,
+  `jversion_id` INT(10) unsigned NOT NULL,
   `source_id` int(10) unsigned NOT NULL,
   `ars_category` int(10) unsigned NOT NULL,
   `group_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_languagepack_languages_group_id` FOREIGN KEY (`group_id`) REFERENCES `#__usergroups` (`id`)  ON DELETE NO ACTION,
-  CONSTRAINT `fk_languagepack_languages_source_id` FOREIGN KEY (`source_id`) REFERENCES `#__languagepack_sources` (`id`)  ON DELETE NO ACTION
+  CONSTRAINT `fk_languagepack_languages_source_id` FOREIGN KEY (`source_id`) REFERENCES `#__languagepack_sources` (`id`)  ON DELETE NO ACTION,
   CONSTRAINT `fk_languagepack_languages_joomla_version` FOREIGN KEY (`jversion_id`) REFERENCES `#__languagepack_jversions` (`id`)  ON DELETE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
@@ -55,3 +55,11 @@ CREATE TABLE IF NOT EXISTS `#__languagepack_releases` (
 INSERT INTO `#__languagepack_sources` (`const`, `name`) VALUES
 ('github', 'LANGUAGE_PACK_SOURCE_GITHUB'),
 ('crowdin', 'LANGUAGE_PACK_SOURCE_CROWDIN');
+
+--
+-- Populate data into the Joomla Versions table
+--
+INSERT INTO `#__languagepack_jversions` (`name`) VALUES
+('COM_LANGUAGE_PACK_JOOMLA_VERSION_1_5'),
+('COM_LANGUAGE_PACK_JOOMLA_VERSION_2_5'),
+('COM_LANGUAGE_PACK_JOOMLA_VERSION_3_X');
