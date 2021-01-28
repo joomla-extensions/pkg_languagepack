4b4732588ace396b616ac2ab45ae0efd4467ae4f
wilsonge <georgejameswilson@googlemail.com>
2021-01-28 21:32:03 +0000

    Fix file uploads failing

--- com_languagepack/site/views/newpack/tmpl/default.php
+++ com_languagepack/site/views/newpack/tmpl/default.php
@@ -35,7 +35,7 @@ Factory::getDocument()->addScriptDeclaration("
 		<?php echo Text::sprintf('COM_LANGUAGE_PACK_NEWPACK_CREATE_RELEASE', $this->languageInfo->name) ?>
 	</h1>
 
-	<form action="<?php echo Route::_('index.php?option=com_languagepack&id=' . (int) $this->item->id); ?>" <?php if ($this->sourceId === 3): ?>enctype="multipart/form-data"<?php endif; ?> method="post" name="adminForm" id="adminForm" class="form-validate form-vertical">
+	<form action="<?php echo Route::_('index.php?option=com_languagepack&id=' . (int) $this->item->id); ?>" <?php if ((int) $this->languageInfo->source_id === 3): ?>enctype="multipart/form-data"<?php endif; ?> method="post" name="adminForm" id="adminForm" class="form-validate form-vertical">
 		<?php echo $this->form->renderField('id'); ?>
 		<?php echo $this->form->renderField('joomla_version'); ?>
 		<?php echo $this->form->renderField('language_pack_version'); ?>
