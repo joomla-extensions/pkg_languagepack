3b050979b0bb50fe92948db8f144e31664ceba6d
George Wilson <georgejameswilson@googlemail.com>
2021-06-17 23:00:22 +0100

    Update com_languagepack/site/models/rules/version.php
    Co-authored-by: Tobias Zulauf <zero-24@users.noreply.github.com>

--- com_languagepack/site/models/rules/version.php
+++ com_languagepack/site/models/rules/version.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  com_languagepack
  *
- * @copyright   Copyright (C) 2020 - 2021 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2021 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
